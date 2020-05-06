d70c39d15945442b7659b8b0f9796532b3922a90
jkk004 <62976976+jkk004@users.noreply.github.com>
2020-05-05 20:44:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('https://gist.githubusercontent.com/johnburnmurdoch/4199dbe55095c3e13de8d5b2e5e5307a/raw/fa018b25c24b7b5f47fd0568937ff6c04e384786/city_populations',
                  usecols=['name', 'group', 'year', 'value'])
-print("What year(1500-2020) would you like to see: ")
+print("What year(1900-2020) would you like to see: ")
 
 choice = int(input())
 final = (data[data['year'].eq(choice)].sort_values(by='value', ascending=True).head(15))
